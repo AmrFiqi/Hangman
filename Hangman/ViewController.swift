620033df0a0834485bef2a785d95f1ba0b3632f3
AmrFiqi <amr.h.elfiqi@gmail.com>
2023-01-18 23:23:33 +0200

    Initial Commit

---
+++ Hangman/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Hangman
+//
+//  Created by Amr El-Fiqi on 17/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+    }
+
+    func loadGame (){
+        
+    }
+}
+
